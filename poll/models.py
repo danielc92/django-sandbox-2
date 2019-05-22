3f2e5889dce8a79be1fd6ea70f9ab62dd4211fa4
Daniel Corcoran <danielc.92@outlook.com>
2019-05-22 16:41:32 +1000

    Added str magic functions

--- poll/models.py
+++ poll/models.py
@@ -7,9 +7,15 @@ class Poll(models.Model):
     poll_creator = models.CharField(max_length=255)
     poll_created = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.poll_question
+
 class Choice(models.Model):
 
     choice_name = models.CharField(max_length=255)
     choice_created = models.DateTimeField(auto_now=True)
     poll_question = models.ForeignKey(Poll, 
                                       on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.choice_name
