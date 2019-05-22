ad6e11cc045edf2b4c02ee06e784de909d684f40
Daniel Corcoran <danielc.92@outlook.com>
2019-05-22 15:51:51 +1000

    Fixed models mistake

--- poll/models.py
+++ poll/models.py
@@ -1,16 +1,14 @@
 from django.db import models
 
 # Create your models here.
-class Choice(models.Model):
-
-    choice_name = models.CharField(max_length=255)
-    choice_created = models.DateTimeField(auto_now=True)
-
-
 class Poll(models.Model):
 
     poll_question = models.CharField(max_length=255)
     poll_creator = models.CharField(max_length=255)
     poll_created = models.DateTimeField(auto_now=True)
 
-    poll_choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+class Choice(models.Model):
+
+    choice_name = models.CharField(max_length=255)
+    choice_created = models.DateTimeField(auto_now=True)
+    poll_question = models.ForeignKey(Poll, on_delete=models.CASCADE)
