a960b80e8a33dd84a4497858cea208c158187f6c
Daniel Corcoran <danielc.92@outlook.com>
2019-05-22 15:26:56 +1000

    Added two models

--- poll/models.py
+++ poll/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Choice(models.Model):
+
+    choice_name = models.CharField(max_length=255)
+    choice_added = models.DateTimeField(auto_now=True)
+
+
+class Poll(models.Model):
+
+    poll_question = models.CharField(max_length=255)
+    poll_creator = models.CharField(max_length=255)
+    poll_created = models.DateTimeField(auto_now=True)
+
+    poll_choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
