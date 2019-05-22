8aed7de268dd96385135a143739a1866207551c2
Daniel Corcoran <danielc.92@outlook.com>
2019-05-22 15:32:24 +1000

    Created make_poll view

--- poll/views.py
+++ poll/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .forms import PollForm, ChoiceForm
 # Create your views here.
+
+def make_poll(request):
+
+    if request.method == 'POST':
+
+        form = PollForm(request.POST):
+
+        if form.is_valid():
+            print('form is valid')
+            return HttpResponse('Success')
+
+    else:
+
+        form = PollForm()
+
+    context = {'form' : form}
+
+    return render(request, 'make_poll.html', context)
